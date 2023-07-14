b64f1ffc108f8c3a67bf9fdf58b762c90efd996f
Banana <mail@bananas-playground.net>
2023-07-14 13:20:50 +0200

    not used anymore. Docker images already deleted

--- test/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source test.env
-docker pull portagefilelist/pfl-base
-docker run -it --rm -v "${PFL_DEV}:/pfl" -v "${PORTAGE}:/var/db/repos/gentoo" portagefilelist/pfl-base /bin/bash
